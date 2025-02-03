64b9414770d3c5c0c7a4a892259bb84f56bee276
Simon D <simon.delves@and.digital>
2025-02-03 11:02:07 +0000

    Modal component

---
+++ src/components/atoms/Modal/Modal.jsx
@@ -0,0 +1,25 @@
+import { Cross2Icon } from "@radix-ui/react-icons";
+import { Dialog } from "radix-ui";
+
+
+export const Modal = ({title, children}) => {
+  return (
+
+    <Dialog.Portal>
+      <Dialog.Overlay className="fixed inset-0 bg-black/75" />
+      <Dialog.Content
+        aria-describedby={undefined}
+        className="bg-white rounded-md fixed inset-1/2 -translate-x-1/2 -translate-y-1/2 p-6 w-full md:w-auto h-full md:h-auto md:min-h-[600px] md:min-w-[500px] overflow-y-auto"
+      >
+        <Dialog.Title>{title}</Dialog.Title>
+        <Dialog.Close asChild>
+          <button className="absolute top-4 right-4 cursor-pointer" aria-label="Close">
+            <Cross2Icon />
+          </button>
+        </Dialog.Close>
+       {children}
+      </Dialog.Content>
+    </Dialog.Portal>
+  )
+}
+
