e92aee141782d356621b924fab5df8fb3e392063
Simon D <simon.delves@and.digital>
2025-02-03 11:03:13 +0000

    Header and cart

---
+++ src/components/molecules/Cart/Cart.jsx
@@ -0,0 +1,23 @@
+import { Modal } from "../../atoms/Modal/Modal";
+
+export const Cart = ({ items = {} }) => {
+  return (
+    <Modal title='Your cart'>
+      {Object.values(items).length ?
+        Object.values(items).map(item => {
+          const { product, count } = item
+
+          return (
+            <div className="flex justify-between w-full pt-2" key={product?.id}>
+
+              <span> {product?.title}</span>
+              <span> {count}</span>
+            </div>)
+        }) :
+        <p className="pt-5">No items in your cart</p>
+      }
+
+    </Modal>
+  )
+}
+
---
+++ src/components/molecules/ProductTable/HeaderBar/HeaderBar.jsx
@@ -0,0 +1,13 @@
+import { PersonIcon } from "@radix-ui/react-icons"
+import { Dialog } from "radix-ui";
+
+export const HeaderBar = () => {
+  return (
+    <div className='flex justify-end h-6 shadow-md'>
+      <Dialog.Trigger className="cursor-pointer px-1">
+        <PersonIcon />
+      </Dialog.Trigger>
+    </div>
+  )
+}
+
